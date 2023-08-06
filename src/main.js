5a621c73de2a7953774a7cc01d7e710025825b22
Ed Centeno <cchavarriacenteno8@gmail.com>
2023-08-06 15:28:46 -0600

    TrendingMovies

--- src/main.js
+++ src/main.js
@@ -1,3 +1,31 @@
 import { API_KEY } from "./secrets.js";
 
-console.log("Hola");
+async function getTrendingMoviesPreview() {
+  const res = await fetch(
+    "https://api.themoviedb.org/3/trending/movie/day?api_key=" + API_KEY
+  );
+  const data = await res.json();
+
+  const movies = data.results;
+  movies.forEach((movie) => {
+    const trendingPrevieMoviesContainer = document.querySelector(
+      ".trendingPreview-movieList"
+    );
+    const movieContainer = document.createElement("div");
+    movieContainer.classList.add("movie-container");
+
+    const movieImg = document.createElement("img");
+    movieImg.classList.add("movie-img");
+    movieImg.setAttribute("alt", movie.title);
+    movieImg.setAttribute(
+      "src",
+      `https://image.tmdb.org/t/p/w300/${movie.poster_path}`
+    );
+
+    movieContainer.appendChild(movieImg);
+    trendingPrevieMoviesContainer.appendChild(movieContainer);
+  });
+  console.log({ data, movies });
+}
+
+getTrendingMoviesPreview();
